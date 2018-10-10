741f455d96ffe66558f38539ebc16e1c4df9f959
phong211297 <39408318+phong211297@users.noreply.github.com>
2018-10-10 19:00:28 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,64 @@
 const express = require('express');
-const path = require('path');
-let app = express();
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
-app.use(express.static('../btvn2'))
+app.use(bodyParser.urlencoded({ extended : false }));
 
 app.get('/', (req,res) => {
-          console.log(__dirname); 
-        //   res.send({success: 1,data:"Hello" });
-        res.sendFile(path.resolve(__dirname, '../btvn2/index.html'));
-});
-app.get('http://localhost:3000/style.css', (req,res) => {
-    console.log(__dirname); 
-  res.sendFile(path.resolve(__dirname, '../btvn2/style.css'));
-});
-app.listen(3000,(err) => {
-    if(err) console.log(err);
-    else console.log("Server is listening at port 3000!");
-    
+    res.sendFile(__dirname + '/public/answer.html');
 })
+
+app.get('/ask', (req,res) => {
+    res.sendFile(__dirname + '/public/ask.html');
+})
+
+app.get('/answer', (req,res) => {
+    res.sendFile(__dirname + '/public/answer.html');
+})
+
+app.post('/creatquestion', (req,res) => {
+    console.log(req.body);
+    //const questionList = fs.readFileSync('./questions.json');
+    let questionList = JSON.parse(fs.readFileSync('./questions.json'));
+    console.log(questionList.length);
+
+    const newQuestion = {
+        id : questionList.length,
+        questionContent : req.body.questionContent,
+        yes : 0,
+        no : 0
+    };
+
+    questionList.push(newQuestion);
+    fs.writeFileSync('./questions.json', JSON.stringify(questionList));
+
+    res.redirect('/answer');
+ });
+
+app.post('/creatquestion', (req,res) => {
+   req.on('data', (data) => {
+       console.log(data);
+   });
+});
+
+const questionListReceived = fs.readFileSync(__dirname + '/questions.json',{encoding : 'utf-8'},(err) =>{
+    if(err) console.log(err)
+     else console.log("write file success!");
+    });
+ data1 = JSON.parse(questionListReceived);
+ len = data1.length;
+ var ran = Math.floor(Math.random() * len);
+ var quesContent = data1[ran].questionContent;
+ fs.writeFileSync('questionRandom.txt' , quesContent , (err)  => {
+     if (err) console.log (err)
+     else console.log("write random success");
+ })
+
+app.use(express.static('public'));
+
+app.listen(3000, (err) => {
+    if(err) console.log(err)
+    else console.log("Server is listening at port 3000");
+
+});
